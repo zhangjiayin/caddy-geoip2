f48b4dee202d0c51498d4faa68597bdb28316c17
Jz <>
2023-02-07 18:21:14 +0800

    fix bug for https://caddyserver.com/account/register-package

--- geoip2_test.go
+++ geoip2_test.go
@@ -3,6 +3,7 @@ package geoip2
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"testing"
 )
 
@@ -11,7 +12,10 @@ import (
 func TestStruct(t *testing.T) {
 	// geoIP2State.DatabaseDirectory = "asdfasdfsd"
 	// v := caddyconfig.JSON(geoIP2State, nil)
+	geoIP2State := GeoIP2State{}
 	jsonStr := "{\"databaseDirectory\":\"dddd\",\"accountId\":333}"
 	dec := json.NewDecoder(bytes.NewReader([]byte(jsonStr)))
 	dec.DisallowUnknownFields()
+	dec.Decode(&geoIP2State)
+	fmt.Printf("%v", geoIP2State)
 }
